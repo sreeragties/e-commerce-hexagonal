2bbdba909d9ae79c7ecca81796d3a0091dcd6bf7
Sreerag T S <33002915+sreeragties@users.noreply.github.com>
2025-04-13 13:19:00 +0530

    feat: first commit

---
+++ src/main/java/com/rage/ecommerce/EcommerceHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.rage.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceHexagonalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rage/ecommerce/EcommerceHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rage.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
