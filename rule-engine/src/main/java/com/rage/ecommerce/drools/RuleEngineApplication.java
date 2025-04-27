7dba41cab5f72583100c4fb262fa71aba22508c3
Sreerag T S <33002915+sreeragties@users.noreply.github.com>
2025-04-27 23:12:14 +0530

    feat: added new drools sub project

---
+++ rule-engine/src/main/java/com/rage/ecommerce/drools/RuleEngineApplication.java
@@ -0,0 +1,13 @@
+package com.rage.ecommerce.drools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuleEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuleEngineApplication.class, args);
+	}
+
+}
