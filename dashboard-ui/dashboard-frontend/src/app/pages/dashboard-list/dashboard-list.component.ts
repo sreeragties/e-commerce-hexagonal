b3f6bb8e77a7f76fac31f6ea4d4a05d917a4b223
Sreerag T S <33002915+sreeragties@users.noreply.github.com>
2025-05-08 23:39:02 +0530

    feat: created home, dashboard-list and dashboard-detail component

---
+++ dashboard-ui/dashboard-frontend/src/app/pages/dashboard-list/dashboard-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-list.component.html',
+  styleUrl: './dashboard-list.component.scss'
+})
+export class DashboardListComponent {
+
+}
