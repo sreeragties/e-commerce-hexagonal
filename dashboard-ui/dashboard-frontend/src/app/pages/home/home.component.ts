92e13301ad74deb6c05e9e8dc4e40befbcdd3e23
Sreerag T S <33002915+sreeragties@users.noreply.github.com>
2025-05-09 14:06:36 +0530

    feat: added spinning animation to button

--- dashboard-ui/dashboard-frontend/src/app/pages/home/home.component.ts
+++ dashboard-ui/dashboard-frontend/src/app/pages/home/home.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    CommonModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  isLoading: boolean = false;
+  customerId: string = '';
+
+  handleSubmit(): void {
+    if (!this.customerId) {
+      console.log("Customer ID is empty");
+      return;
+    }
+
+    this.isLoading = true;
+    console.log('Submitting Customer ID:', this.customerId);
+
+    setTimeout(() => {
+      this.isLoading = false;
+      console.log('Processing complete for:', this.customerId);
+    }, 3000);
+  }
+
+  ngOnInit(): void {
+    console.log('Initial isLoading state in ngOnInit:', this.isLoading);
+  }
 
 }
