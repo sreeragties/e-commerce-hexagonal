7c6ff937e41ce745dc48f8b33b50b011f821c9f3
Sreerag T S <33002915+sreeragties@users.noreply.github.com>
2025-05-09 17:54:09 +0530

    feat: added html code to order-tracker.component.html

--- dashboard-ui/dashboard-frontend/src/app/app.routes.ts
+++ dashboard-ui/dashboard-frontend/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
-import {DashboardListComponent} from './pages/dashboard-list/dashboard-list.component';
 import {OrderTrackerComponent} from './pages/order-tracker/order-tracker.component';
 
 export const routes: Routes = [
