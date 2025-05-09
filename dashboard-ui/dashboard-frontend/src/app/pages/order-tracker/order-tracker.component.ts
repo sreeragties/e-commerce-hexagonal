7e1e6dea5e496f3f104bac5df1468adc4f1ea2fc
Sreerag T S <33002915+sreeragties@users.noreply.github.com>
2025-05-09 19:58:01 +0530

    feat: logic added to update UI based on order state

--- dashboard-ui/dashboard-frontend/src/app/pages/order-tracker/order-tracker.component.ts
+++ dashboard-ui/dashboard-frontend/src/app/pages/order-tracker/order-tracker.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
-import {NgIf} from "@angular/common";
+import { Component, Input } from '@angular/core';
+import {NgClass, NgIf} from "@angular/common";
 import {FormsModule} from '@angular/forms';
 import {MatIcon} from '@angular/material/icon';
+
+export enum OrderState {
+  CREATED = 'CREATED',
+  OFFER_APPLIED = 'OFFER_APPLIED',
+  PAYMENT_APPROVED = 'PAYMENT_APPROVED',
+  PROCESSING = 'PROCESSING',
+  SHIPPED = 'SHIPPED',
+  DELIVERED = 'DELIVERED',
+  COMPLETED = 'COMPLETED',
+}
 
 @Component({
   selector: 'app-order-tracker',
@@ -10,13 +20,40 @@ import {MatIcon} from '@angular/material/icon';
     NgIf,
     FormsModule,
     MatIcon,
+    NgClass
   ],
   templateUrl: './order-tracker.component.html',
   styleUrl: './order-tracker.component.scss'
 })
 export class OrderTrackerComponent {
 
+  @Input() currentOrderState: OrderState | null = OrderState.PAYMENT_APPROVED;
+
   isLoading: boolean = false;
   customerId: string = '';
+
+  OrderState = OrderState;
+
+  isStepComplete(stepState: OrderState): boolean {
+    if (this.currentOrderState === null) {
+      return false;
+    }
+    const currentStateIndex = Object.values(OrderState).indexOf(this.currentOrderState);
+    const stepStateIndex = Object.values(OrderState).indexOf(stepState);
+    return currentStateIndex > stepStateIndex;
+  }
+
+  isStepActive(stepState: OrderState): boolean {
+    return this.currentOrderState === stepState;
+  }
+
+  isLineComplete(stepState: OrderState): boolean {
+    if (this.currentOrderState === null) {
+      return false;
+    }
+    const currentStateIndex = Object.values(OrderState).indexOf(this.currentOrderState);
+    const stepStateIndex = Object.values(OrderState).indexOf(stepState);
+    return currentStateIndex > stepStateIndex;
+  }
 
 }
