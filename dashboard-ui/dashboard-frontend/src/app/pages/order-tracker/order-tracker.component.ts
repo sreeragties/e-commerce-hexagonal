3b8dbed22f703c1fe948307dcd1920ecc75aae1a
Sreerag T S <33002915+sreeragties@users.noreply.github.com>
2025-05-09 19:35:18 +0530

    feat: added spinners logic and further css

--- dashboard-ui/dashboard-frontend/src/app/pages/order-tracker/order-tracker.component.ts
+++ dashboard-ui/dashboard-frontend/src/app/pages/order-tracker/order-tracker.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import {NgIf} from "@angular/common";
 import {FormsModule} from '@angular/forms';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-order-tracker',
   standalone: true,
-    imports: [
-        NgIf,
-        FormsModule,
-    ],
+  imports: [
+    NgIf,
+    FormsModule,
+    MatIcon,
+  ],
   templateUrl: './order-tracker.component.html',
   styleUrl: './order-tracker.component.scss'
 })
